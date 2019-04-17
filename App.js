d2b8e60ab9b2fffab8feb7f5be33f696067dbba2
Arun Kumar Kandregula <kandregula.arunkumar@gmail.com>
2019-04-16 23:20:56 -0700

    Implemented live search

--- App.js
+++ App.js
@@ -6,24 +6,79 @@
  * @flow
  */
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import React, { Component } from "react";
+import { StyleSheet, Text, View, TextInput } from "react-native";
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+const DATA_URL = "http://localhost:8080/places";
+// eslint-disable-next-line react/prefer-stateless-function
+export default class App extends Component {
+  state = {
+    places: [],
+    searchText: ""
+  };
 
-type Props = {};
-export default class App extends Component<Props> {
+  componentDidMount() {
+    /* global fetch */
+    fetch(DATA_URL)
+      .then(resp => resp.json())
+      .then(resp => {
+        this.setState({
+          places: resp
+        });
+      });
+  }
+
+  renderRestaurantList = () => {
+    const map = this.state.places
+      .filter(
+        eachPlace =>
+          !this.state.searchText ||
+          eachPlace.name
+            .toLowerCase()
+            .includes(this.state.searchText.toLowerCase())
+      )
+      .map((eachPlace, i) => {
+        return (
+          <View
+            style={[
+              styles.restaurantRow,
+              { backgroundColor: i % 2 === 0 ? "white" : "#F3F3F7" }
+            ]}
+            key={eachPlace.name}
+          >
+            <View style={styles.edges}>
+              <Text>{eachPlace.id}</Text>
+            </View>
+            <View style={styles.middleColumn}>
+              <Text>{eachPlace.name}</Text>
+              <Text style={{ color: "grey" }}>{eachPlace.address}</Text>
+            </View>
+            <View style={styles.edges}>
+              <Text>info</Text>
+            </View>
+          </View>
+        );
+      });
+    return map;
+  };
+
+  handleChangeText = searchText => {
+    this.setState({
+      searchText
+    });
+  };
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        <Text style={styles.header}>Restaurant Reviews2</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Enter search text..."
+          onChangeText={this.handleChangeText}
+          value={this.state.searchText}
+        />
+        <View style={styles.restaurantList}>{this.renderRestaurantList()}</View>
       </View>
     );
   }
@@ -32,18 +87,31 @@ export default class App extends Component<Props> {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: "#F5FCFF"
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  header: {
+    fontSize: 30,
+    textAlign: "center",
+    padding: 40,
+    color: "#0066CC"
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  restaurantRow: {
+    flexDirection: "row"
   },
+  middleColumn: {
+    flex: 8
+  },
+  edges: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  input: {
+    padding: 10,
+    fontSize: 16,
+    borderBottomWidth: 1,
+    borderColor: "#ddd",
+    backgroundColor: "#F5F5F5",
+    marginBottom: 30
+  }
 });
--- README.md
+++ README.md
@@ -2,5 +2,10 @@
 
 # Previous Steps:
 
-# Current Step:
 1. Scaffolded a brand new react-native project.
+
+# Current Step:
+
+2. Implemented a simple live search for restaurants from a server.
+   - Dont forget to run json-server -p 8080 src/tests/fixtures/restaurants.json to serve the mock data for the list of restaurants.
+   - Doesnt YET support scrolling. Will be implemented in next step.
