e9f5269eca7508c5c91a09707e72fe057e9c5104
Arun Kumar Kandregula <kandregula.arunkumar@gmail.com>
2019-04-19 16:04:51 -0700

    Turned Info Text to simple Button

--- README.md
+++ README.md
@@ -5,9 +5,16 @@
 1. Scaffolded a brand new react-native project.
 
 2. Implemented a simple live search for restaurants from a server.
+
    - Dont forget to run json-server -p 8080 src/tests/fixtures/restaurants.json to serve the mock data for the list of restaurants.
    - Doesnt YET support scrolling. Will be implemented in next step.
 
+3. Implemented the list using FlatList, which is memory performant.
+
 # Current Step:
 
-3. Implemented the list using FlatList, which is memory performant.
+4. Turn Last column <Text>Info<Text> to button.
+
+- Button will turn it into button
+  - One small issue is the flex:1 we gave to styles.edges may not be enough to fit a button so info may get wrapped. To fix it we may add minWidth=50
+  - But Button element dont have those many config options.
--- src/components/RestaurantRow.js
+++ src/components/RestaurantRow.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, StyleSheet, Text } from 'react-native';
+import { View, StyleSheet, Text, Button } from 'react-native';
 
 const styles = StyleSheet.create({
   restaurantRow: {
@@ -11,29 +11,52 @@ const styles = StyleSheet.create({
   edges: {
     flex: 1,
     justifyContent: "center",
-    alignItems: "center"
+    alignItems: "center",
+    minWidth: 50
   }
 });
 
-const RestaurantRow = ({ place, index }) => {
-  return <View
-    style={[
-      styles.restaurantRow,
-      { backgroundColor: index % 2 === 0 ? "white" : "#F3F3F7" }
-    ]}
-    key={place.name}
-  >
-    <View style={styles.edges}>
-      <Text>{place.id}</Text>
-    </View>
-    <View style={styles.middleColumn}>
-      <Text>{place.name}</Text>
-      <Text style={{ color: "grey" }}>{place.address}</Text>
-    </View>
-    <View style={styles.edges}>
-      <Text>info</Text>
-    </View>
-  </View>
+
+class RestaurantRow extends React.Component {
+  state = {
+    showInfo: false
+  }
+  handlePress = () => {
+    this.setState({
+      showInfo: !this.state.showInfo
+    });
+  }
+  render() {
+    const { place, index } = this.props;
+
+    return <View styles={{ backgroundColor: index % 2 === 0 ? "white" : "#F3F3F7" }}>
+      <View
+        style={styles.restaurantRow}
+        key={place.name}
+      >
+        <View style={styles.edges}>
+          <Text>{place.id}</Text>
+        </View>
+        <View style={styles.middleColumn}>
+          <Text>{place.name}</Text>
+          <Text style={{ color: "grey" }}>{place.address}</Text>
+        </View>
+        <View style={styles.edges}>
+          <Button
+            title="info"
+            accessibilityLabel="info"
+            color="#004567"
+            onPress={this.handlePress}
+          />
+        </View>
+      </View>
+      {
+        this.state.showInfo && <View>
+          <Text>{place.name}</Text>
+        </View>
+      }
+    </View >
+  }
 }
 
 export default RestaurantRow;
